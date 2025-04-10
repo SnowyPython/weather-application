bae06f764ca57f7ec9ef243a17d006db636171a0
SnowyPython <135994019+SnowyPython@users.noreply.github.com>
2025-04-10 23:38:27 +0500

    fix css

--- src/main/java/ru/vatolin/weatherapplication/config/WebConfig.java
+++ src/main/java/ru/vatolin/weatherapplication/config/WebConfig.java
@@ -8,7 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**").addResourceLocations("/images/").setCachePeriod(0);
-        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(0);
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/")
+                .setCachePeriod(0);
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/")
+                .setCachePeriod(0);
     }
 }
