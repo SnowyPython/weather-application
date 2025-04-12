397fbeb1eb610de6f78ba64ddb32c5c814d6ab31
SnowyPython <135994019+SnowyPython@users.noreply.github.com>
2025-04-12 14:15:44 +0500

    UI fixes

--- src/main/java/ru/vatolin/weatherapplication/dto/MainLocationDto.java
+++ src/main/java/ru/vatolin/weatherapplication/dto/MainLocationDto.java
@@ -9,6 +9,7 @@ public class MainLocationDto {
     private String codeCountry;
     private double feels;
     private String main;
+    private String icon;
     private String humidity;
 
     public void convert(RawLocationDto rawLocationDto) {
@@ -17,6 +18,7 @@ public class MainLocationDto {
         this.codeCountry = rawLocationDto.getSys().getCodeCountry();
         this.feels = rawLocationDto.getMain().getFeels();
         this.main = rawLocationDto.getWeathers().getFirst().getMain();
+        this.icon = rawLocationDto.getWeathers().getFirst().getIcon();
         this.humidity = rawLocationDto.getMain().getHumidity();
     }
 }
